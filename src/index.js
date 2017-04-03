116a522ea788aaa751a17f04430f50636943288c
audouze <v.audouze@laposte.net>
2017-04-03 14:46:46 +0200

    modified index to set json export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+fetch('assets/test.json')
+  .then(function(response) {
+    if (!response.ok) return new Error(response);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+  });
+
 
 class MyClass {
   constructor() {
