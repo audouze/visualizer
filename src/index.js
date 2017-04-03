7e358d64c7b85cd8b39f0daeba3ea0ac3bd1de2f
audouze <v.audouze@laposte.net>
2017-04-03 16:16:27 +0200

    added d3 libraries

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-fetch('assets/test.json')
+fetch('assets/data.json')
   .then(function(response) {
     if (!response.ok) return new Error(response);
     return response.json();
@@ -8,20 +8,3 @@ fetch('assets/test.json')
   });
 
 
-class MyClass {
-  constructor() {
-    console.log(this);
-  }
-
-  * generatorMethod() {
-    let i = 0;
-
-    while (i < 10)
-      yield i++;
-
-    return 'done';
-  }
-}
-
-const myClass = new MyClass();
-console.log([...myClass.generatorMethod()]);
